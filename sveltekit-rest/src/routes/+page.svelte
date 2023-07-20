f99601b23e0baea1648f8b545f210582a40cfbef
Allan Anderson <rubel@goosemoon.org>
2023-07-19 20:32:23 -1000

    Go back to simple individual Rest call

--- sveltekit-rest/src/routes/+page.svelte
+++ sveltekit-rest/src/routes/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-    import type { PageData } from "./$types";
-    export let data: PageData
+	import type { PageData } from './$types';
+	export let data: PageData;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <ul>
-    {#each data.issues.data as issue}
-    <li> <a href="{issue.html_url}">{issue.title}</a> </li>
-    {/each}
-
+	{#each data.issues.data as issue}
+		<li><a href={issue.html_url}>{issue.title}</a></li>
+	{/each}
 </ul>
 {data.issues.headers.link}
